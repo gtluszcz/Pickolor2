b32ed5ea88bd55be6fc6918034e52115e391e263
gtluszcz <gtluszcz@gamil.com>
2017-11-28 15:19:54 +0100

    Convert to use Heroku

--- config/database.php
+++ config/database.php
@@ -1,5 +1,13 @@
 <?php
 
+    $url = parse_url(getenv("DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+    echo $url;
+
+
 return [
 
     /*
@@ -13,7 +21,9 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'pgsql',
+
+
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +66,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
