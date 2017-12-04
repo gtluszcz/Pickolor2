2ef93efb56236498efe4aa6f946ff5343f849e2c
gtluszcz <gtluszcz@gamil.com>
2017-12-04 21:43:52 +0100

    register

--- resources/views/assets/sidebar.blade.php
+++ resources/views/assets/sidebar.blade.php
@@ -1,9 +1,14 @@
+
 <div class="sidemenu-shown sidemenu-hidden">
     <div class="sidemenu-bar logo onlydesktop"><a href="/" >Pickolor</a></div>
     <div class="userlogo">
         <span class="glyphicon glyphicon-user"></span>
     </div>
-    <div class="sidemenu-bar"><a class="thick" href="/login">sign in </a>/<a class="thick" href=""> sign up</a></div>
+    @if (!Auth::check())
+    <div class="sidemenu-bar"><a class="thick" href="/login">sign in </a>/<a class="thick" href="/register"> sign up</a></div>
+    @else
+        <div class="sidemenu-bar">Auth::user()->name</div>
+    @endif
     <div class="sidemenu-bar side-menu-link onlymobile"><a href="/palette" >Palets</a></div>
     <div class="sidemenu-bar side-menu-link onlymobile"><a href="#" >Colors</a></div>
     <div class="sidemenu-bar side-menu-link onlymobile"><a href="#" >Gradients</a></div>
